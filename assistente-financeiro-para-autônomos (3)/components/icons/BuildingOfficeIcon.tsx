37b9a0662ad9cbb73256b52f635de71627853026
Thales2209 <thales_2209@hotmail.com>
2025-08-05 08:36:13 -0300

    Delete assistente-financeiro-para-autônomos (3) directory

--- assistente-financeiro-para-autônomos (3)/components/icons/BuildingOfficeIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const BuildingOfficeIcon: React.FC = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-slate-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 4h5m-5 4h5m-5-4h.01M9 7h.01M13 7h.01M13 11h.01M13 15h.01" />
-    </svg>
-);
